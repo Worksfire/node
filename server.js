a5f6dcf0dd196dea53adcbae31966bbe5509df95
patrick gorrell <patrick@patricks-MBP.home>
2016-01-10 21:19:14 -0800

    setting up a node rest api

--- server.js
+++ server.js
@@ -10,13 +10,17 @@ var PORT = process.env.PORT || 3000;
 app.use(middleware.logger);
 
 // Log about page specificly
-app.get('/about', middleware.requireAuthentication, function(req, res) {
-    res.send('About Us');
-});
+//app.get('/about', middleware.requireAuthentication, function(req, res) {
+//    res.send('About Us');
+//});
 
 // Use any html file in the public folder
-app.use(express.static(__dirname + '/public'));
+//app.use(express.static(__dirname + '/public'));
+
+app.get('/', function() {
+    res.send('Todo API Root');
+});
 
 app.listen(PORT, function() {
-    console.log('Express server started on port:' + PORT);
+    console.log('Express listening on port:' + PORT);
 });
