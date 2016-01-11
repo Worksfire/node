c8f175e6a8fe581ad81a328fd468d7cc995f9530
patrick gorrell <patrick@worksfire.com>
2016-01-10 22:44:42 -0800

    todo list push

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 // Requires
 var express = require('express');
 var middleware = require('./middleware');
+var todos = [{
+    id: 1,
+    description: 'Meet mom for lunch',
+    completed: false,
+}, {
+    id: 2,
+    description: 'Go to the market',
+    completed: false
+}, {
+    id: 3,
+    description: 'Create a business directory and profit',
+    completed: true
+}];
 
 // Server Vars
 var app = express();
@@ -21,6 +34,30 @@ app.get('/', function(req, res) {
     res.send('Todo API Root');
 });
 
+// GET /todos
+app.get('/todos', function(req, res) {
+    res.json(todos);
+});
+
+// Get /todos/:id
+app.get('/todos/:id', function(req, res) {
+    var todoID = parseInt(req.params.id);
+    var matchedTodo;
+
+    todos.forEach(function(todo){
+        if(todoID === todo.id) {
+            matchedTodo = todo;
+        }
+    });
+
+    if(matchedTodo) {
+        res.json(matchedTodo);
+    } else {
+        res.status(404).send();
+    }
+});
+
+// Start the server
 app.listen(PORT, function() {
     console.log('Express listening on port:' + PORT);
 });
