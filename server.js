59355d615d0467f1e8b77a6eca4faf024d8fbdb9
patrick gorrell <patrick@worksfire.com>
2016-01-11 20:24:34 -0800

    removable tasks

--- server.js
+++ server.js
@@ -63,6 +63,19 @@ app.post('/todos', function(req, res) {
     res.json(todos);
 });
 
+// DELETE /todos/:id
+app.delete('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+
+    if(!matchedTodo) {
+        res.status(404).json({"error": "no todo found with that ID"});
+    } else {
+        todos = _.without(todos, matchedTodo);
+        res.json(matchedTodo);
+    }
+});
+
 // Start the server
 app.listen(PORT, function() {
     console.log('Express listening on port:' + PORT);
