6001b9a5b3b60f3f93f58ca2247abb635975949b
patrick gorrell <patrick@worksfire.com>
2016-01-13 14:18:12 -0800

    database for sqlite

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require('express');
 var middleware = require('./middleware');
 var bodyParser = require('body-parser');
 var _ = require('underscore');
-
+var db = require('./db.js');
 // Server Vars
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -63,19 +63,23 @@ app.get('/todos/:id', function(req, res) {
 
 // POST /todos
 app.post('/todos', function(req, res) {
-
     var body = _.pick(req.body, 'description', 'completed');
 
-    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-       return res.status(400).send('You are missing a value');
-    }
-
-    body.description = body.description.trim();
-
-    body.id = todoNextId++;
-
-    todos.push(body);
-    res.json(todos);
+    db.todo.create(body).then(function(todo) {
+        res.json(todo.toJSON());
+    }, function (e) {
+        res.status(400).json(e);
+    });
+    //if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
+    //   return res.status(400).send('You are missing a value');
+    //}
+    //
+    //body.description = body.description.trim();
+    //
+    //body.id = todoNextId++;
+    //
+    //todos.push(body);
+    //res.json(todos);
 });
 
 // DELETE /todos/:id
@@ -120,7 +124,9 @@ app.put('/todos/:id', function(req, res) {
     res.json(matchedTodo);
 });
 
-// Start the server
-app.listen(PORT, function() {
-    console.log('Express listening on port:' + PORT);
+db.sequelize.sync().then(function() {
+    // Start the server
+    app.listen(PORT, function() {
+        console.log('Express listening on port:' + PORT);
+    });
 });
