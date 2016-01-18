a31b400f3fc8ae602e4f80f0810f10891513c095
patrick gorrell <patrick@worksfire.com>
2016-01-18 13:27:47 -0800

    filtered task creation by user

--- server.js
+++ server.js
@@ -66,7 +66,11 @@ app.post('/todos', middleware.requireAuthentication, function(req, res) {
     var body = _.pick(req.body, 'description', 'completed');
 
     db.todo.create(body).then(function(todo) {
-        res.json(todo.toJSON());
+        req.user.addTodo(todo).then(function() {
+            return todo.reload();
+        }).then(function(todo) {
+            res.json(todo.toJSON());
+        });
     }, function (e) {
         res.status(400).json(e);
     });
@@ -151,7 +155,7 @@ app.post('/users/login', function(req, res) {
 });
 
 // USE {force:true} inside sync() to drop tables and rebuild
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
     // Start the server
     app.listen(PORT, function() {
         console.log('Express listening on port:' + PORT);
